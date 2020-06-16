6a6b8764b321f1a1b8480dc1570761e9c15ef7ae
yangbo_vendo <yangbo_vendor@sensetime.com>
2020-06-16 16:55:23 +0800

    feat:完成命令式组件生成，完成router和store根据目录进行查找

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
 Vue.use(Vuex);
+const modules = {};
+
+const context = require.context("../views", true, /store\/index\.js$/);
+context.keys().forEach(key => {
+  modules[key.replace(/^\.\/|\/store\/index\.js$/g, "")] = {
+    namespaced: true,
+    ...context(key).default
+  };
+});
+
+console.info(modules);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: modules,
+  strict: true
 });
